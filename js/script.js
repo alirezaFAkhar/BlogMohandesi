7fcb15d0117557f3d10c8c89ba96ee7a09b65346
alirezafakhar <avatar_ali95@yahoo.com>
2021-06-06 16:05:03 +0430

    v1-8

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const btn = document.querySelector('.toggle-menu');
-const list = document.querySelector('nav ul');
-console.log(list)
+const btn = document.querySelector(".toggle-menu");
+const list = document.querySelector("nav ul");
+console.log(list);
 
-btn.addEventListener('click', function() {
-    list.classList.toggle('active');
-    btn.classList.toggle('active');
-})
+btn.addEventListener("click", function () {
+  list.classList.toggle("active");
+  btn.classList.toggle("active");
+});
